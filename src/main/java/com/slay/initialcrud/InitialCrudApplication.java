8c9fb3a53ea89fa87a7a6c00a3a6a24d0aec90e9
Sulaiman Khawar <s.khawar@chello.at>
2020-08-05 23:08:20 +0200

    first commit

---
+++ src/main/java/com/slay/initialcrud/InitialCrudApplication.java
@@ -0,0 +1,13 @@
+package com.slay.initialcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitialCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slay/initialcrud/InitialCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slay.initialcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
