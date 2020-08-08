b8ae7b66088111fcf069d9a24570136fa79413f2
Sulaiman Khawar <s.khawar@chello.at>
2020-08-08 23:29:23 +0200

    added serializer for Json date

--- src/main/java/com/slay/initialcrud/config/WebConfig.java
+++ src/main/java/com/slay/initialcrud/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.slay.initialcrud.config;
 
+import com.slay.initialcrud.interceptor.BaseInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
